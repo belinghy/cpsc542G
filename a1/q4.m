fc1562f6b4f62c6b9f22bcb4eb96b1fa6d17cb32
Ben Ling <belinghy@gmail.com>
2018-02-10 10:54:40 -0800

    Done Q4

--- a1/q4.m
+++ a1/q4.m
@@ -1,28 +1,67 @@
 % Newton's Method for Boundary Value Problem
-n = 8;
-tol = 1.e-8;
-iter_max = 32;
+tol = 1.e-10;
+iter_max = 10;
 
-x = linspace(1,2,n+2);
-sol = log(x);
-sol = sol(2:n+1);
-y = zeros(n,1);
-y(n) = log(2);
+figure
+ax1 = subplot(3,1,1);
+xlabel('x')
+ylabel('y')
+ax2 = subplot(3,1,2);
+ylabel('ln(relative error)')
+ax3 = subplot(3,1,3);
+xlabel('Num. Iterations')
+ylabel('ln(norm(f))')
 
-for i=1:iter_max
-  fy = f(y); 
-  Jy = J(y);
-  fprintf ('%d: %e \n', i-1, norm(fy))
-  dy = -Jy\fy; % search direction = p
-  y = y + dy;
+hold(ax1,'on')
+hold(ax2,'on')
+hold(ax3,'on')
+for n=2.^linspace(3,6,4)
+  x = linspace(1, iter_max+1, iter_max+1);
+  [err, norm_fy, fx, result, solution] = finite_diff(n, tol, iter_max);
+  plot(ax1, fx, solution, '-')
+  plot(ax1, fx, result, 'o')
+  plot(ax2, x, log(err))
+  plot(ax3, x, log(norm_fy))
+end
+legend('n=8','16','32','64')
+
+% Actual Newton's Method 
+function [r_err, r_norm_fy, fx, result, solution]=finite_diff(n, tol, iter_max)
+
+  x = linspace(1,2,n+2);
+  sol = log(x);
+  sol = sol(2:n+1);
+  y = zeros(n,1); % initial guess
+
+  r_err = zeros(1, iter_max+1);
+  r_norm_fy = zeros(1, iter_max+1);
   
-  norm_p = norm(dy);
-  norm_y = norm(y);
-  fprintf('\t norm(p)=%0.10f; tol*(1+norm(x))=%0.10f\n', norm_p, tol*(1+norm_y))
-  if norm_p < tol*(1+norm_y)
-    fy = f(y);
-    fprintf ('%d: %e \n', i, norm(fy))
-    return
+  for i=1:iter_max
+    fy = f(y); 
+    Jy = J(y);
+    err = sum(abs(y.' - sol)/abs(sol));
+    r_err(i) = err;
+    r_norm_fy(i) = norm(fy);
+    fprintf('%d: %e \n', i-1, norm(fy))
+    fprintf('%d: %e \n', i-1, err)
+    dy = -Jy\fy; % search direction = p
+    y = y + dy;
+
+    norm_p = norm(dy);
+    norm_y = norm(y);
+    fprintf('\t norm(p)=%0.10f; tol*(1+norm(x))=%0.10f\n', norm_p, tol*(1+norm_y))
+    if norm_p < tol*(1+norm_y) || i == iter_max
+      fy = f(y);
+      err = sum(abs(y.' - sol)/abs(sol));
+      r_err(i+1) = err;
+      r_norm_fy(i+1) = norm(fy);
+      result = y.';
+      solution = sol;
+      fx = x(2:n+1);
+      fprintf('%d: %e \n', i, norm(fy))
+      fprintf('%d: %e \n', i, sum(abs(y.' - sol)/abs(sol)))
+      return
+    end
   end
 end
 
