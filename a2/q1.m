04499514a02562093e0c917c4a1da87181876613
Ben Ling <belinghy@gmail.com>
2018-03-15 20:08:18 -0700

    Figure and bugfixes

--- a2/q1.m
+++ a2/q1.m
@@ -1,7 +1,3 @@
-% Langrange seems to be best choice
-% Monomial is not stable enough
-% Don't need adaptive of Newton
-
 f = @(x) cos(30*pi*x);
 f_xi = @(i,n) cos((2*i+1)/(2*n+2)*pi);
 x = -1:.001:1;
@@ -11,7 +7,7 @@ figure;
 hold on;
 plot(x, y_real);
 
-ns = 10:10:80;
+ns = 10:10:170;
 errors = zeros(1,length(ns));
 loop_index = 1;
 for n = ns
--- a2/q1_test.m
+++ a2/q1_test.m
@@ -1,20 +1,27 @@
-f = @(x) x.^2;
-f_xi = @(n) linspace(1,2,n);
-x = 1:.001:2;
+f = @(x) cos(30*pi*x);
+f_xi = @(i,n) cos((2*i+1)/(2*n+2)*pi);
+x = -1:.001:1;
 y_real = f(x);
 
 figure;
 hold on;
 plot(x, y_real);
 
-for n = 1:4
-  xi = f_xi(n);
+ns = 10:10:80;
+errors = zeros(1,length(ns));
+loop_index = 1;
+for n = ns
+  xi = f_xi(0:n,n);
   A = vander(xi); % Vandermonde, flipped
-  y = f(xi);
-  coeffs = A\y';
-  
+  yi = f(xi);
+  [coeffs,fl] = gmres(A,yi',size(A,1),1e-5); % backslash blows up  
   y_interp = polyval(coeffs,x); % Polyval expect flipped
-  error = abs(y_real-y_interp);
+  errors(loop_index) = max(abs(y_real-y_interp));
   plot(x, y_interp);
-  pause;
+  loop_index = loop_index + 1;
 end
+
+figure;
+semilogy(ns, errors);
+xlabel('$n\ (degree)$','Interpreter','latex')
+ylabel('$max.\ error$','Interpreter','latex')
