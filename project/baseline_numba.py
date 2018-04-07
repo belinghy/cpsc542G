425579a188fd1f5b2350f17d520bcbd6a1dc158a
Ben Ling <belinghy@gmail.com>
2018-04-07 15:29:50 -0700

    Changed advect to use numba, it is the last major bottleneck

--- project/baseline_numba.py
+++ project/baseline_numba.py
@@ -13,6 +13,28 @@ from utils import timeSince
 
 
 @jit(void(float64[:],
+          float64, float64, float64, float64, float64,
+          float64[:], float64[:],
+          float64))
+def _set_block(v, vw, vh, vdx, vox, voy, top_left, bot_right, value):
+    """Sets the fluid quantity within top_left and bot_right rectangle
+    to have value returned by f.
+
+    top_left: (x0, y0)
+    bot_right: (x1, y1)
+    f: ()
+    """
+    ix0 = int(top_left[0]/vdx - vox)
+    iy0 = int(top_left[1]/vdx - voy)
+    ix1 = int(bot_right[0]/vdx - vox)
+    iy1 = int(bot_right[1]/vdx - voy)
+
+    for iy in range(max(iy0, 0), min(iy1, vh)):
+        for ix in range(max(ix0, 0), min(ix1, vw)):
+            v[ix+iy*vw] = value
+
+
+@jit(void(float64[:],
           float64[:], uint32,
           float64[:], uint32,
           uint32, uint32, float64), nopython=True)
@@ -112,6 +134,74 @@ def _make_incompressible(u, uw, v, vw, pressure, w, h, dx, rho, timestep):
         v[ix+h*vw] = 0
 
 
+@jit(float64(float64, float64, float64))
+def two_point_interpolate(a, b, x):
+    """Simple linear interpolation"""
+    return a*(1-x) + b*x
+
+
+@jit(float64(float64, float64,
+             float64[:], uint32, uint32, float64, float64))
+def _mac_grid_interpolate(x, y, w, ww, wh, wox, woy):
+    """2D linear interpolate the quantity values at (x, y) from four
+    near by grid points.
+    """
+    # Boundary checking
+    x = min(max(x - wox, 0.0), ww - 1.001)
+    y = min(max(y - woy, 0.0), wh - 1.001)
+    # Extract integer and fractional parts
+    ix = int(x)
+    iy = int(y)
+    x -= ix
+    y -= iy
+
+    x00 = w[(ix+0)+(iy+0)*ww]
+    x10 = w[(ix+1)+(iy+0)*ww]
+    x01 = w[(ix+0)+(iy+1)*ww]
+    x11 = w[(ix+1)+(iy+1)*ww]
+
+    return two_point_interpolate(
+        two_point_interpolate(x00, x10, x),
+        two_point_interpolate(x01, x11, x), y)
+
+
+@jit(float64(float64[:], float64[:],
+             uint32, uint32, float64, float64, float64,
+             float64,
+             float64[:], uint32, uint32, float64, float64,
+             float64[:], uint32, uint32, float64, float64))
+def _advect(w, wbuf,
+            ww, wh, wox, woy, wdx,
+            timestep,
+            u, uw, uh, uox, uoy,
+            v, vw, vh, vox, voy):
+    """Advect the fluid quantity use velocity fields of u, v
+
+    Eq. (3.6) to (3.9).  This is the Lagrangian part of the
+    semi-Lagrangian methods.
+    """
+    idx = 0
+    for iy in range(wh):
+        for ix in range(ww):
+            x = ix + wox
+            y = iy + woy
+
+            # Forward Euler method
+            # Divide by dx because we want an index value
+            x -= _mac_grid_interpolate(x, y, u, uw,
+                                       uh, uox, uoy) * timestep / wdx
+            y -= _mac_grid_interpolate(x, y, v, vw,
+                                       vh, vox, voy) * timestep / wdx
+
+            # We now know the value at the next time step should be
+            # the old value at (x, y), however this might not be perfectly
+            # on a grid point, so we interpolate again.
+            # Writing to buf[] because we still need to use the old values
+            # to advect other quantities.
+            wbuf[idx] = _mac_grid_interpolate(x, y, w, ww, wh, wox, woy)
+            idx += 1
+
+
 class FluidQuantity:
     """Stores quantities like density field, velocity field, etc
     on a staggered MAC grid
@@ -144,30 +234,13 @@ class FluidQuantity:
         self._val, self._buf = self._buf, self._val
 
     def advect(self, timestep, u, v):
-        """Advect the fluid quantity use velocity fields of u, v
-
-        Eq. (3.6) to (3.9).  This is the Lagrangian part of the
-        semi-Lagrangian methods.
+        """Calls kernel function and writes to self._buf
         """
-        idx = 0
-        for iy in range(self._h):
-            for ix in range(self._w):
-                # Need these in this loop because they'll get changed
-                x = ix + self._ox
-                y = iy + self._oy
-
-                # Forward Euler method
-                # Divide by dx because we want an index value
-                x -= u.linear_interpolate(x, y) * timestep / self._dx
-                y -= v.linear_interpolate(x, y) * timestep / self._dx
-
-                # We now know the value at the next time step should be
-                # the old value at (x, y), however this might not be perfectly
-                # on a grid point, so we interpolate again.
-                # Writing to buf[] because we still need to use the old values
-                # to advect other quantities.
-                self._buf[idx] = self.linear_interpolate(x, y)
-                idx += 1
+        _advect(self._val, self._buf,
+                self._w, self._h, self._ox, self._oy, self._dx,
+                timestep,
+                u._val, u._w, u._h, u._ox, u._oy,
+                v._val, v._w, v._h, v._ox, v._oy)
 
     def linear_interpolate(self, x, y):
         """2D linear interpolate the quantity values at (x, y) from four
@@ -193,23 +266,6 @@ class FluidQuantity:
 
         return lerp(lerp(x00, x10, x), lerp(x01, x11, x), y)
 
-    def apply_block(self, top_left, bot_right, f):
-        """Sets the fluid quantity within top_left and bot_right rectangle
-        to have value returned by f.
-
-        top_left: (x0, y0)
-        bot_right: (x1, y1)
-        f: ()
-        """
-        ix0 = (int)(top_left[0]/self._dx - self._ox)
-        iy0 = (int)(top_left[1]/self._dx - self._oy)
-        ix1 = (int)(bot_right[0]/self._dx - self._ox)
-        iy1 = (int)(bot_right[1]/self._dx - self._oy)
-
-        for iy in range(max(iy0, 0), min(iy1, self._h)):
-            for ix in range(max(ix0, 0), min(ix1, self._w)):
-                self.apply(ix, iy, f)
-
 
 class BaselineFluidSolver:
     """ FluidSolver implements a semi-lagrangian method for solving
@@ -219,7 +275,7 @@ class BaselineFluidSolver:
     for solving the pressure equation.
     """
 
-    def __init__(self, size, fluid_density, fluid_quantities):
+    def __init__(self, size, fluid_density):
         """
         fluid_quantities is a list of quantities to advect
         """
@@ -232,7 +288,8 @@ class BaselineFluidSolver:
 
         # Fluid quantities
         # _u, _v are the x, y velocities on the border of a grid
-        self._quantities = fluid_quantities
+        self._p = FluidQuantity(size=(self._w, self._h),
+                                offset=(0.5, 0.5), dx=_dx)
         self._u = FluidQuantity(size=(self._w+1, self._h),
                                 offset=(0.0, 0.5), dx=_dx)
         self._v = FluidQuantity(size=(self._w, self._h+1),
@@ -253,33 +310,21 @@ class BaselineFluidSolver:
         self.advect(timestep)
 
     def calc_neg_div(self):
-        """Eq. (4.11) and (4.13)
-        This construction is trivially parallizable, no reason to do it in a loop
-        """
+        """ Docs in kernel function"""
         _calc_neg_div(self._neg_div,
                       self._u._val, self._u._w,
                       self._v._val, self._v._w,
                       self._w, self._h, self._dx)
 
     def project(self, timestep, tol=1e-5, max_iter=600):
-        """Eq. (4.19) Calculate pressure from divergence
-
-        The pressure equation is a standard Laplacian, conjugate gradient is the method
-        of choice for SPD matrices.
-
-        This methods implements a Gauss-Seidel solver, mostly for ease of implementation
-        """
+        """ Docs in kernel function"""
         iters, error = _project(self._pressure, self._neg_div, self._w, self._h,
                                 self._dx, self._rho, timestep, tol, max_iter)
         print('Project finished in %d iterations, final error is %g' %
               (iters, error))
 
     def make_incompressible(self, timestep):
-        """Equation (4.4) and (4.5)
-
-        Pressure should be updated using project(), now we calculate fluid velocities,
-        to make the fluid incompressible.
-        """
+        """ Docs in kernel function"""
         _make_incompressible(self._u._val, self._u._w,
                              self._v._val, self._v._w,
                              self._pressure,
@@ -290,39 +335,45 @@ class BaselineFluidSolver:
         """Calculate fluid quantity values at the next time step.
         """
         # Advect all quantities
-        for _, key in enumerate(self._quantities):
-            self._quantities[key].advect(timestep, self._u, self._v)
+        self._p.advect(timestep, self._u, self._v)
         # Advect u and v themselves
         self._u.advect(timestep, self._u, self._v)
         self._v.advect(timestep, self._u, self._v)
 
         # Swap _val[] and _buf[] to commit result of advection
-        for _, key in enumerate(self._quantities):
-            self._quantities[key].swap()
+        self._p.swap()
         self._u.swap()
         self._v.swap()
 
-    def set_condition(self, conditions):
+    def set_condition(self):
         """Sets the condition of the simulation environment.
-
-        It's hard to make this general... Right now hard coding this.
         """
-        def extract_parts(key_value):
-            """Helper function"""
-            return key_value['block'][0:2], key_value['block'][2:4], key_value['func']
 
-        for _, key in enumerate(self._quantities):
-            if key in conditions:
-                top_left, bot_right, func = extract_parts(conditions[key])
-                self._quantities[key].apply_block(top_left, bot_right, func)
+        # Source 1
+        _set_block(self._p._val,
+                   self._p._w, self._p._h, self._p._dx, self._p._ox, self._p._oy,
+                   [0.20, 0.2], [0.30, 0.21], 1.0)
 
-        if 'u' in conditions:
-            top_left, bot_right, func = extract_parts(conditions['u'])
-            self._u.apply_block(top_left, bot_right, func)
+        _set_block(self._u._val,
+                   self._u._w, self._u._h, self._u._dx, self._u._ox, self._u._oy,
+                   [0.20, 0.2], [0.30, 0.21], 0.0)
 
-        if 'v' in conditions:
-            top_left, bot_right, func = extract_parts(conditions['v'])
-            self._v.apply_block(top_left, bot_right, func)
+        _set_block(self._v._val,
+                   self._v._w, self._v._h, self._v._dx, self._v._ox, self._v._oy,
+                   [0.20, 0.2], [0.30, 0.21], 3.0)
+
+        # Source 2
+        _set_block(self._p._val,
+                   self._p._w, self._p._h, self._p._dx, self._p._ox, self._p._oy,
+                   [0.70, 0.2], [0.80, 0.21], 1.0)
+
+        _set_block(self._u._val,
+                   self._u._w, self._u._h, self._u._dx, self._u._ox, self._u._oy,
+                   [0.70, 0.2], [0.80, 0.21], 0.0)
+
+        _set_block(self._v._val,
+                   self._v._w, self._v._h, self._v._dx, self._v._ox, self._v._oy,
+                   [0.70, 0.2], [0.80, 0.21], 3.0)
 
 
 def main():
@@ -340,39 +391,8 @@ def main():
     # A buffer that stores a RGB PNG image to be outputted
     pixels = np.zeros((SIZE_X, SIZE_Y, 3), dtype=np.uint8)
 
-    fluid_quantities = {
-        'particle_density': FluidQuantity(
-            size=(SIZE_X, SIZE_Y), offset=(0.5, 0.5), dx=1/min(SIZE_X, SIZE_Y))
-    }
-
-    def unsigned_set_func_wrapper(target_val):
-        """Returns a function f: (cur_val) -> target_val"""
-        def wrapped(cur_val):
-            """Returns target_val is target_val is larger"""
-            if abs(cur_val) < abs(target_val):
-                return target_val
-            else:
-                return cur_val
-        return wrapped
-
-    conditions = {
-        'particle_density': {
-            'block': (0.45, 0.2, 0.55, 0.21),
-            'func': unsigned_set_func_wrapper(1.0)
-        },
-        'u': {
-            'block': (0.45, 0.2, 0.55, 0.21),
-            'func': unsigned_set_func_wrapper(0.0)
-        },
-        'v': {
-            'block': (0.45, 0.2, 0.55, 0.21),
-            'func': unsigned_set_func_wrapper(3.0)
-        }
-    }
-
     fluid_solver = BaselineFluidSolver(
-        size=(SIZE_X, SIZE_Y), fluid_density=FLUID_DENSITY,
-        fluid_quantities=fluid_quantities)
+        size=(SIZE_X, SIZE_Y), fluid_density=FLUID_DENSITY)
 
     def print_image(particle_density, image, filename):
         """Outputs a PNG using particle_density FluidQuantity"""
@@ -390,11 +410,11 @@ def main():
     img_index = 0
     start_time = time.time()
     for step in range(1, N_STEPS+1):
-        fluid_solver.set_condition(conditions)
+        fluid_solver.set_condition()
         fluid_solver.step(TIMESTEP)
 
         if step % PRINT_EVERY == 0:
-            print_image(fluid_quantities['particle_density'],
+            print_image(fluid_solver._p,
                         pixels, 'output/frame{:05d}.png'.format(img_index))
             img_index += 1
 
